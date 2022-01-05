915cc4d04fa744a4afb01d4cbb82c87717b6e87c
mukerem <mukeremali112@gmail.com>
2022-01-05 12:01:14 +0300

    fix version control

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'cs1andalus',         # How you named your package folder (MyLib)
   packages = ['cs1andalus'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'cs1 graphical modules for educational purposes',   # Give a short description about your library
   author = 'Mukerem Ali',                   # Type in your name
   author_email = 'mukeremali112@domain.com',      # Type in your E-Mail
   url = 'https://github.com/mukerem/cs1andalus',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mukerem/cs1andalus/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/mukerem/cs1andalus/archive/refs/tags/v1.0.tar.gz',    # I explain this later on
   install_requires=[        
           'Pillow',
       ],
